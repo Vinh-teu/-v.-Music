f84ecbb0d4bf518bc50fbc60a0c4ee61b0d9926f
Vinh-teu <ngvinh20105@gmail.com>
2025-02-27 02:08:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "vi",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "mongodb+srv://vinh:<Vinhteu123>@cluster0.4kwla.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  mongodbUri : "mongodb+srv://vinh:<Teuvinh123>@cluster0.4kwla.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
